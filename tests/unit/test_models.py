9dfca4b9da154ae54f516e1d21db4acbc8f5e36e
sjdoepker <sjdoepker@uchicago.edu>
2024-06-04 18:37:10 -0500

    set up basic tests, issues with importing modules so the linter isn't going to pass

---
+++ tests/unit/test_models.py
@@ -0,0 +1,54 @@
+# pylint: disable=redefined-outer-name
+"""Unit tests for db models"""
+import json
+import pytest
+
+from sqlalchemy.exc import IntegrityError
+
+from watchlist.models import User, Show, Entry
+
+# pylint: disable=unused-import
+from tests.conftest import test_db, test_user, test_show, test_entry
+
+def test_new_user_created(db):
+    """
+    Tests creation of new user, makes sure all the fields are set correctly
+    """
+    new_user = db.session.execute(db.select(User)).first()
+    assert new_user.id == 1
+    assert new_user.email == "testmail@email.com"
+    assert new_user.display_name == "John Doe"
+    # Hardcoded because pw isn't retrievable from User object
+    assert new_user.pw_valid("password123") is True
+
+def test_unique_email_required(test_user):
+    """
+    Checks that trying to create a user with an already used email raises an IntegrityError
+    """
+    data = {
+        "email": test_user.email,
+        "display_name":"Jane Doe",
+        "pw": "password456"
+    }
+    with pytest.raises(IntegrityError):
+        User(json.dumps(data))
+
+def test_new_show_created(db, test_show):
+    """
+    Tests creation of a new show, makes sure all the fields are set correctly
+    """
+    new_show = db.session.execute(db.select(Show)).first()
+    assert new_show.id == test_show.id
+    assert new_show.title == test_show.title
+
+def test_new_entry_created(db, test_user, test_show, test_entry):
+    """
+    Tests creation of a new entry, makes sure all the fields are set correctly including
+    foreign keys. UNFINISHED
+    """
+    new_entry = db.session.execute(db.select(Entry)).first()
+    assert new_entry.id == 1
+    assert new_entry.date_added == test_entry.date_added
+    assert new_entry.user_id == test_user.user_id
+    assert new_entry.show_id == test_show.show_id
+    # not yet finished, more fields to check
