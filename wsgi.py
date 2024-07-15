b226c7020e15df1e15a24a9d17c92f0825cc0d4f
Sam Doepker <sjdoepker@uchicago.edu>
2024-07-15 10:55:06 -0500

    added wsgi.py locally, deleted unnecessary conftest file

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from src.app import app
+
+if __name__ == '__main__':
+    app.run(debug=False)
+
+#for running the server on windows (if you need to for some reason) use waitress, eg:
+# waitress-serve --threads=1 --listen=127.0.0.1:8000 src:app
+# (currrently broken but it shouldn't matter)
